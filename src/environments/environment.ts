b21f29f284cfa60f8b865435809ba3c1152862ea
tanprograma <tanprograma@gmaol.com>
2023-12-26 14:49:54 +0300

    changed data in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  title: 'stigoRX',
-  medicines_url: 'https://julyfifteenapi.onrender.com/api/medicines',
-  clients_url: 'https://julyfifteenapi.onrender.com/api/clients',
-  inventories_url: 'https://julyfifteenapi.onrender.com/api/inventories',
-  stores_url: 'https://julyfifteenapi.onrender.com/api/stores',
-  units_url: 'https://julyfifteenapi.onrender.com/api/units',
-  suppliers_url: 'https://julyfifteenapi.onrender.com/api/suppliers',
+  title: 'stigoRX 24',
+  medicines_url: 'https://pharmacy2024.onrender.com/api/medicines',
+  clients_url: 'https://pharmacy2024.onrender.com/api/clients',
+  inventories_url: 'https://pharmacy2024.onrender.com/api/inventories',
+  stores_url: 'https://pharmacy2024.onrender.com/api/stores',
+  units_url: 'https://pharmacy2024.onrender.com/api/units',
+  suppliers_url: 'https://pharmacy2024.onrender.com/api/suppliers',
 };
